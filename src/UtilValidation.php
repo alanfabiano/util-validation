36078ec24a5ba2f8359366232ca18b89ae770d7c
Alan Milani <alan.fabiano@gmail.com>
2016-07-16 00:47:04 -0300

    Fix Validation Cpf

--- src/UtilValidation.php
+++ src/UtilValidation.php
@@ -38,7 +38,8 @@ class UtilValidation
     */
     public static function Cpf($attribute, $value)
     {
-        if ($value == '00000000000' || $value == '11111111111' || $value == '22222222222' || $value == '33333333333' || $value == '44444444444' || $value == '55555555555' || $value == '66666666666' || $value == '77777777777' || $value == '88888888888' || $value == '99999999999')
+        $value = preg_replace('/[^0-9]/', '', $value);
+        if (strlen($value) < 11 || strlen($value) > 11 || $value == '00000000000' || $value == '11111111111' || $value == '22222222222' || $value == '33333333333' || $value == '44444444444' || $value == '55555555555' || $value == '66666666666' || $value == '77777777777' || $value == '88888888888' || $value == '99999999999')
         {
             return false;
         }else{
