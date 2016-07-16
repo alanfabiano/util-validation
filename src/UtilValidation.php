65f09beed0ee149517c44b83ae7ce708efc44b13
Alan Milani <alan.fabiano@gmail.com>
2016-07-16 00:28:31 -0300

    Fix Validate of Cpf

--- src/UtilValidation.php
+++ src/UtilValidation.php
@@ -36,7 +36,7 @@ class UtilValidation
      * @param  mixed   $value
      * @return bool
     */
-    public static function Cpf()
+    public static function Cpf($attribute, $value)
     {
         if ($value == '00000000000' || $value == '11111111111' || $value == '22222222222' || $value == '33333333333' || $value == '44444444444' || $value == '55555555555' || $value == '66666666666' || $value == '77777777777' || $value == '88888888888' || $value == '99999999999')
         {
